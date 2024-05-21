2c9bd493fe6c3026ce05c0cbc45995ede6e7286e
iinsue <hinsu93@naver.com>
2024-05-21 23:37:04 +0900

    add hono

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,17 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+// c 는 context 라는 의미
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+// file based routing
+export const GET = handle(app);
+export const POST = handle(app);
