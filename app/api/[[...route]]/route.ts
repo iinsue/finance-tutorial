35533856b9e4162c9f78eef86502690839b68e1c
iinsue <hinsu93@naver.com>
2024-05-22 00:04:59 +0900

    zod + hono

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
@@ -12,14 +14,23 @@ app
       message: "Hello Next.js!",
     });
   })
-  .get("/hello/:test", (c) => {
-    const test = c.req.param("test");
+  .get(
+    "/hello/:test",
+    zValidator(
+      "param",
+      z.object({
+        test: z.number(),
+      })
+    ),
+    (c) => {
+      const { test } = c.req.valid("param");
 
-    return c.json({
-      message: "Hello World",
-      test: test,
-    });
-  });
+      return c.json({
+        message: "Hello World",
+        test: test,
+      });
+    }
+  );
 
 // file based routing
 export const GET = handle(app);
