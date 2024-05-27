f05e07c5018c36b21f44969189272ae3d76f884b
insu <hinsu93@gmail.com>
2024-05-27 15:52:17 +0900

    header 에 user button 추가

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,7 @@
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 import { HeaderLogo } from "./header-logo";
 import { Navigation } from "./navigation";
+import { Loader2 } from "lucide-react";
 
 export const Header = () => {
   return (
@@ -11,6 +13,12 @@ export const Header = () => {
               <HeaderLogo />
               <Navigation />
             </div>
+            <ClerkLoaded>
+              <UserButton afterSignOutUrl="/" />
+            </ClerkLoaded>
+            <ClerkLoading>
+              <Loader2 className="size-8 animate-spin text-slate-400" />
+            </ClerkLoading>
           </div>
         </div>
       </header>
